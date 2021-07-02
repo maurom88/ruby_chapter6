3784c406d1e8e09a22de9171e2510b43f1baaec1
Mauro Meden <mauro.meden@shopify.com>
2021-07-02 09:06:24 -0600

    Find array elements

--- reviews.rb
+++ reviews.rb
@@ -14,5 +14,12 @@ File.open("reviews.txt") do |review_file|
     lines = review_file.readlines
 end
 
-puts "Line 4: #{lines[3]}"
-puts "Line 1: #{lines[0]}"
+relevant_lines = []
+
+lines.each do |line|
+    if line.include?("Truncated")
+        relevant_lines << line
+    end
+end
+
+puts relevant_lines
