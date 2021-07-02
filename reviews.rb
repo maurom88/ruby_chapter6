5e757f236bde97c6bcb93bd9f1cbf89756a21541
Mauro Meden <mauro.meden@shopify.com>
2021-07-02 11:28:45 -0600

    Create array of adjectives

--- reviews.rb
+++ reviews.rb
@@ -9,6 +9,7 @@
 # review_file.close
 
 lines = []
+adjectives = []
 
 File.open("reviews.txt") do |review_file|
     lines = review_file.readlines
@@ -17,4 +18,14 @@ end
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 reviews = relevant_lines.reject { |line| line.include?("--") }
 
-puts reviews
+def find_adjective(string)
+    words = string.split(" ")
+    index = words.find_index("is")
+    words[index + 1]
+end
+
+reviews.each do |review|
+    adjectives << find_adjective(review)
+end
+
+puts adjectives
